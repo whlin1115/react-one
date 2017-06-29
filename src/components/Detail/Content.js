8b3241f4e017b941d4aa7706d12937eabdb68b3e
wenhuan.lin <moerslion@163.com>
2017-06-29 19:21:29 +0800

    add something

--- src/components/Detail/Content.js
+++ src/components/Detail/Content.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Footer from './Footer';
 import Header from './Header';
 import './style.css';
+import './one.css';
 
 function Content({ detail }) {
   return (
     <div className="normal">
       <Header />
-      <div className="content">
+      <div className="text-content">
         <h2 className="title">{detail.hp_title}</h2>
-        <p className="author">文 / {detail.author[0].user_name} </p>
         <div dangerouslySetInnerHTML={{ __html: detail.hp_content }} />
       </div>
       <Footer />
